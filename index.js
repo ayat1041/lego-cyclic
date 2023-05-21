746c068fac3dc9f3807d2f76c1c6ca4e79ff37dc
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-22 03:44:26 +0600

    updated env

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.port || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+// https://brainy-purse-boa.cyclic.app
 
 // MONGODB
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ichdzcf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `${process.env.URI}`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
